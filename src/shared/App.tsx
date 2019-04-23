e45ad34229c7a68e5e89d11f1340ab3b500409bb
5tarlight <developerstarlight@gmail.com>
2019-04-23 23:33:10 +0900

    clean

---
+++ src/shared/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {}
+
+const Portal: React.FunctionComponent<Props> = () => {
+  return (
+    <div>
+      <h2>
+        홈
+      </h2>
+    </div>
+  )
+}
+
+export default Portal
