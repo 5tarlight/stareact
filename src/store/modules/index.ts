60286a175d1b1e35607a9254dd006c97c565311c
5tarlight <developerstarlight@gmail.com>
2019-04-26 00:13:59 +0900

    redux ready

---
+++ src/store/modules/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import counter, { CounterState } from './counter'
+
+export default combineReducers(counter)
+
+export interface StoreState {
+  counter: CounterState
+}
