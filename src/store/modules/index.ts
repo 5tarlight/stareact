df4588d28ed1a5abc79044802d9eff1275a5564f
5tarlight <developerstarlight@gmail.com>
2019-04-30 21:18:47 +0900

    update

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,8 +1,13 @@
-import { combineReducers } from 'redux'
-import counter, { CounterState } from './counter'
+import { combineReducers } from 'redux';
+import todos, { TodosState } from './todos';
 
-export default combineReducers(counter)
+export default combineReducers({
+  counter,
+  todos,
+});
 
+// 스토어의 상태 타입 정의
 export interface StoreState {
-  counter: CounterState
+  counter: CounterState;
+  todos: TodosState;
 }
